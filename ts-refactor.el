34c49a65e0db024b7d237e3ff77802675f69ab69
clin <clin@thoughtworks.com>
2019-06-05 23:00:49 +0800

    [Chen] add second function tsr-extract-function

--- ts-refactor.el
+++ ts-refactor.el
@@ -16,3 +16,26 @@
     (newline)
 		)
 	)
+
+(defun tsr-extract-function ()
+   "extract region to an arrow function"
+   (interactive)
+   (tsr--extract-fun-between (region-beginning) (region-end) "const"))
+
+(defun tsr--extract-fun-between (beg end keyword)
+  (let (
+        (expression (buffer-substring beg end))
+        (name "name"))
+
+    (delete-region beg end)
+    (insert name)
+
+    (goto-char 0)
+		(insert (format "%s %s" keyword name))
+    (insert " = () => {")
+    (newline)
+		(insert expression)
+		(newline)
+		(insert "}")
+		)
+	)
